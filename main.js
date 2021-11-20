dc9c8f4715177b461544abd3b22a94d889233a9d
Yenifer <guajeyenifer@gmail.com>
2021-11-20 18:06:25 -0500

    Se crea la funcion anonima donde se almacena la clase Bar y posteriormente se implementa la logica correspondiente para dibujar las barras del juego

--- main.js
+++ main.js
@@ -21,6 +21,22 @@
 })();
 
 (function() {
+    self.Bar = function(x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+    self.Bar.prototype = {
+        down: function() {},
+        up: function() {}
+    }
+})();
+
+(function() {
     self.BoardView = function(canvas, board) {
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -28,12 +44,34 @@
         this.board = board;
         this.ctx = this.canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function() {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el);
+            }
+        }
+    }
+
+    function draw(ctx, element) {
+        switch (element.kind) {
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height)
+                break;
+        }
+    }
 })();
 
 window.addEventListener("load", main);
 
 function main() {
     var board = new Board(800, 400);
+    var bar = new Bar(0, 150, 20, 100, board);
+    var bar = new Bar(780, 150, 20, 100, board);
     var canvas = document.getElementById("canvas");
     var board_view = new BoardView(canvas, board);
+    console.log(board);
+    board_view.draw();
 }
